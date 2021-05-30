4d743de81d3cf07b4d73a29a0b3889a9c5de58f5
Ahoo Wang <ahoowang@qq.com>
2021-05-30 15:07:10 +0800

    ## :tada: :tada: :tada: :tada: :tada: :tada: 1. Project name changed to **CoSky**(**C**on**s**ul + S**ky**)!!! :tada: 2. Release 1.0.0 version, production-level verification!!! :tada:

--- cosky-dashboard/e2e/src/app.e2e-spec.ts
+++ cosky-dashboard/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('govern-dashboard app is running!');
+    expect(await page.getTitleText()).toEqual('cosky-dashboard app is running!');
   });
 
   afterEach(async () => {
