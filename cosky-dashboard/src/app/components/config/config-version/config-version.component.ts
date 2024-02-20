274269c3f11f5833a060fa920f72db5017edf8eb
Ahoo Wang <ahoowang@qq.com>
2024-02-20 14:40:38 +0800

    Update Config Diff

--- cosky-dashboard/src/app/components/config/config-version/config-version.component.ts
+++ cosky-dashboard/src/app/components/config/config-version/config-version.component.ts
@@ -29,7 +29,7 @@ export class ConfigVersionComponent implements OnInit {
   configHistory: ConfigHistoryDto = Configs.ofHistory();
   configCurrent!: ConfigDto;
   configCurrentCode: string = '';
-  lang!: string;
+  lang: string = "yaml";
   @Output() rollbackAfter: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor(private namespaceContext: NamespaceContext,
@@ -43,11 +43,11 @@ export class ConfigVersionComponent implements OnInit {
     this.configClient.getConfigHistory(this.namespaceContext.ensureCurrentNamespace(), this.configId, this.version)
       .subscribe(configHistory => {
         this.configHistory = configHistory;
-      });
-    this.configClient.getConfig(this.namespaceContext.ensureCurrentNamespace(), this.configId)
-      .subscribe(config => {
-        this.configCurrent = config;
-        this.configCurrentCode = config.data;
+        this.configClient.getConfig(this.namespaceContext.ensureCurrentNamespace(), this.configId)
+          .subscribe(config => {
+            this.configCurrent = config;
+            this.configCurrentCode = config.data;
+          });
       });
   }
 
