a94fefc959c46312166648c66986c242d4b2a2dc
Ahoo Wang <ahoowang@qq.com>
2025-12-16 17:34:50 +0800

    feat(config): add config version table with expandable rows (#792)
    - Import ConfigVersionTable component in ConfigPage
    - Implement expandedRowRender to show config versions
    - Add expandable property to Table component
    - Rename ConfigVersionsView to ConfigVersionTable
    - Update ConfigVersionTable to fetch versions via useQuery
    - Remove unnecessary props and close button from version table
    - Adjust table rowKey and column definitions for versions display

---
+++ dashboard/src/pages/config/ConfigVersionTable.tsx
@@ -0,0 +1,44 @@
+/*
+ * Copyright [2021-present] [ahoo wang <ahoowang@qq.com> (https://github.com/Ahoo-Wang)].
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Table} from 'antd';
+import {useQuery} from "@ahoo-wang/fetcher-react";
+import {configApiClient} from "../../services/clients.ts";
+import {ConfigVersion} from "../../generated";
+
+interface ConfigVersionTableProps {
+    namespace: string;
+    configId: string;
+}
+
+export function ConfigVersionTable({namespace, configId}: ConfigVersionTableProps) {
+    const {loading, result: versions} = useQuery<string, ConfigVersion[]>({
+        query: configId,
+        execute: (query, attributes, abortController) => {
+            return configApiClient.getConfigVersions(namespace, query, attributes, abortController);
+        }
+    })
+    const columns = [
+        {title: 'Config Id', dataIndex: 'configId', key: 'configId'},
+        {title: 'Version', dataIndex: 'version', key: 'version'},
+    ];
+
+    return (
+        <Table
+            dataSource={versions}
+            rowKey="configId"
+            columns={columns}
+            loading={loading}
+        />
+    );
+}
