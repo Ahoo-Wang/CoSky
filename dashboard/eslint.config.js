c91570958a4538fc62bb64965446ea7a30935ec3
Ahoo Wang <ahoowang@qq.com>
2026-04-08 19:23:05 +0800

    chore(deps): update fetcher dependencies and add react compiler plugin
    - Updated @ahoo-wang/fetcher packages from 3.13.15 to 3.15.0
    - Updated @ahoo-wang/fetcher-generator from 3.15 to 3.15.0
    - Added eslint-plugin-react-compiler dependency
    - Integrated react-compiler config into eslint configuration
    - Updated typescript-eslint from 8.58.0 to 8.58.1
    - Updated vite from 8.0.5 to 8.0.7

--- dashboard/eslint.config.js
+++ dashboard/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactCompiler from "eslint-plugin-react-compiler";
 import tseslint from 'typescript-eslint'
 import {defineConfig, globalIgnores} from 'eslint/config'
 
@@ -15,6 +16,7 @@ export default defineConfig([
             tseslint.configs.recommended,
             reactHooks.configs.flat.recommended,
             reactRefresh.configs.vite,
+            reactCompiler.configs.recommended
         ],
         languageOptions: {
             ecmaVersion: 2020,
